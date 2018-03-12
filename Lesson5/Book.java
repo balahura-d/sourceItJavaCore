2362f3ef5f1f9716411343a25dd64ea5013af71f
balahura-d <balagura.d.v@gmail.com>
2018-03-12 12:34:12 +0200

    Now... We have a Book! with parameters

---
+++ Lesson5/Book.java
@@ -0,0 +1,14 @@
+
+public class Book {
+	static int numberOfBooks;
+	private int id;
+	String name;
+	String autors;
+	String publisher;
+	long year;
+	int numberOfPages;
+	double price;
+	enum binding {
+		HARD, SOFT
+	}
+}
