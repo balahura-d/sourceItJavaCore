01e6d80721875a45678b1148d59fe7cce08e7a6d
balahura-d <balagura.d.v@gmail.com>
2018-03-30 11:36:20 +0300

    DefaultMyList have all implementations of abstract MyList's method; added get(int) method

--- src/lesson9/homework9/DefaultMyList.java
+++ src/lesson9/homework9/DefaultMyList.java
@@ -2,47 +2,102 @@ package lesson9.homework9;
 
 import java.util.ArrayList;
 
-public class DefaultMyList<T> implements MyList {
-	
+public class DefaultMyList<T> implements MyList<T> {
+
 	ArrayList<T> obj = new ArrayList<T>();
 	ArrayList<Integer> numOfObj = new ArrayList<Integer>();
-		
+
 	@Override
-	public void add(Object e) {
+	public void add(T t) {
+		if (obj.contains(t)) { // если в обж уже есть элемент,
+			numOfObj.set(obj.indexOf(t), // то по его индексу
+					(numOfObj.get(obj.indexOf(t) + 1))); // инкрементим значение его кол-ва
+		} else { // если же нет -
+			obj.add(t); // добавляем в список
+			numOfObj.set(obj.size() - 1, 1); // и указываем, что он первый
+		}
 	}
 
 	@Override
-	public void clear() {
+	public void clear() { // прост))
+		obj.clear();
+		numOfObj.clear();
 	}
 
 	@Override
-	public boolean remove(Object o) {
-		return false;
+	public boolean remove(Object t) {
+		if ((obj.contains(t)) & (numOfObj.get(obj.indexOf(t)) > 1)) { // если элементов больше одного
+			numOfObj.set(obj.indexOf(t), // то по индексу элемена
+					numOfObj.get(obj.indexOf(t) - 1)); // мы записываем меньшее на 1 количество
+			return true; // и всё норм
+		} else if ((obj.contains(t)) & (numOfObj.get(obj.indexOf(t)) == 1)) { // если элемент остался 1
+			numOfObj.remove(obj.indexOf(t)); // удаляем инфо о его количестве (сначала, потому что ищем по
+								// индексу)
+			obj.remove(t); // затем удаляем сам элемент (он единственный по логике списка)
+			return true;
+		} else { // других вариантов быть не должно
+			return false;
+		}
+	}
+
+	public boolean removeAll(Object t) {
+		if ((obj.contains(t)) & (numOfObj.get(obj.indexOf(t)) >= 1)) {
+			numOfObj.remove(obj.indexOf(t));
+			obj.remove(t);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public Object[] toArray() {
-		return null;
+		return obj.toArray(); // прост))
+	}
+
+	public Object[] toArrayAll() {
+		int size = sizeAll();
+		// T[] objArray = (T[])Array.newInstance(getClass(T), 10);
+		Object[] objArray = new Object[size];
+		for (int i = 0; i < size;) { // проходим по результативному циклу
+			for (int j = 0; j < numOfObj.get(obj.indexOf(i)); j++) { // и "numOfObj" раз
+				objArray[i + j] = obj.get(i); // записываем объект
+			} // после чего
+			i += numOfObj.get(obj.indexOf(i)); // переходим на следующую пустую позицию
+		}
+		return objArray;
 	}
 
 	@Override
-	public int size() {
-		return 0;
+	public int size() { // прост))
+		return obj.size();
+	}
+
+	public int sizeAll() { // тут считаем сумму всех значений количества элементов
+		return numOfObj.stream().mapToInt(n -> (int) n).sum();
 	}
 
 	@Override
-	public boolean contains(Object o) {
-		return false;
+	public boolean contains(Object t) { // подсмотрено у ArrayList
+		return obj.indexOf(t) >= 0; // его же (AL) и используем)
 	}
 
 	@Override
-	public boolean containsAll(MyList c) {
-		return false;
+	public boolean containsAll(MyList<?> ml) { // подсмотрено в AbstractCollection и -не- используем его
+		// for (Object e : ml) // Оп! А вот и итератор "всплыл"
+		// if (!contains(e)) // "Can only iterate over an array or an instance of java.lang.Iterable"
+		// return false;
+		// return true;
+		for (int i = 0; i < ml.size(); i++) {
+			if (!obj.contains(ml.get(i)))
+				return false;
+		}
+		return true;
 	}
-	
+
 	@Override
-	public String toString() {
-		return null;
+	public T get(int index) {
+		return obj.get(index);
 	}
 
 }
--- src/lesson9/homework9/MyList.java
+++ src/lesson9/homework9/MyList.java
@@ -1,17 +1,20 @@
 package lesson9.homework9;
 
-public interface MyList {
-	void add(Object e); // appends the specified element to the end of this list
+public interface MyList<T> {
+	
+	void add(T t); // appends the specified element to the end of this list
 
 	void clear(); // removes all of the elements from this list
 
-	boolean remove(Object o); // removes the first occurrence of the specified element from this list
+	boolean remove(T t); // removes the first occurrence of the specified element from this list
 
 	Object[] toArray(); // returns an array containing all of the elements in this list in proper sequence
 
 	int size(); // returns the number of elements in this list
 
-	boolean contains(Object o); // returns true if this list contains the specified element.
+	boolean contains(T t); // returns true if this list contains the specified element.
 
-	boolean containsAll(MyList c); // returns true if this list contains all of the elements of the specified list
+	boolean containsAll(MyList<?> ml); // returns true if this list contains all of the elements of the specified list
+	
+	T get(int index);
 }
