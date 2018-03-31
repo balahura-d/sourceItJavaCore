7f2e5e145c9cef57465364f395742056eab04e6c
balahura-d <balagura.d.v@gmail.com>
2018-03-31 21:46:11 +0300

    ClockDegrees can calculate difference between the all hands of clock

--- src/lesson9/homework9/ClockDegrees.java
+++ src/lesson9/homework9/ClockDegrees.java
@@ -4,96 +4,120 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.regex.*;
+//import java.util.Calendar;
+//import java.util.GregorianCalendar;
 
 public class ClockDegrees {
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
-	
-		String s;
-	//	String[] ss = {"11:11am","1.11aM","12 23 pM","11:11:11","1:11","23:75","12,12","12.12AM","11:11PM","11:11:11Am"};
-	//	int hour;
-	//	int minute;
-	//	double time = 15.5;
-	//	double minuteDegrees;
-	//	double hourDegrees;
-		ArrayList<String> iii = new ArrayList<>();
-		ArrayList<Integer> ii2 = new ArrayList<>();
-		
+
+		String sTime;
+		ArrayList<Integer> hhmmss = new ArrayList<>();
+		// Calendar c = new GregorianCalendar();
+		Date d = new Date();
 
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));) {
-			System.out.print("Enter time:");
-			
-			Pattern p = Pattern.compile("\\d{1,2}[ :\\.,]{1}\\d{2}([ :\\.,]{1}\\d{2})?( ?(p|P|a|A)(m|M))?");
-			Matcher m = p.matcher(s = br.readLine());
-//			for (int i = 0; i < s.length(); i++) {
-//								Matcher m = p.matcher(ss[i]);
-//			}
-//				System.out.print(ss[i]);
-			System.out.print(s);
-			System.out.println(m.matches()?" подходит":" не подходит");
-			Pattern p1 = Pattern.compile("\\d+");
-			Matcher m1 = p1.matcher(s); 
-			while (m1.find()) {
-			   iii.add(m1.group());
-			   ii2.add(Integer.valueOf(m1.group()));
+			System.out.print("Enter time here: ");
+
+			Pattern pTime = Pattern
+					.compile("\\d{1,2}[ :\\.,]{1}\\d{2}([ :\\.,]{1}\\d{2})?( ?(p|P|a|A)(m|M))?");
+			Matcher mTime = pTime.matcher(sTime = br.readLine());
+
+			if (mTime.matches()) {
+				Pattern pDigits = Pattern.compile("\\d+");
+				Matcher mDigits = pDigits.matcher(sTime);
+				while (mDigits.find()) {
+					hhmmss.add(Integer.valueOf(mDigits.group()));
+				}
+
+				// c.set(Calendar.HOUR_OF_DAY, hhmmss.get(0));
+				// c.set(Calendar.MINUTE, hhmmss.get(1));
+				d.setHours(hhmmss.get(0)); // ну вот удобнее через дату получилось
+				d.setMinutes(hhmmss.get(1));
+				if (hhmmss.size() == 3) { // если пользователь вводил и секунды, то массив хранит 3
+								// значения
+					// c.set(Calendar.SECOND, hhmmss.get(2));
+					d.setSeconds(hhmmss.get(2));
+				} else { // если не вводил - по-умолчанию считаем 0 секунд
+					d.setSeconds(0);
+				}
+
+				System.out.println(d);
+				System.out.printf("%.2f\n", getDegreesH(d));
+				System.out.printf("%.2f\n", getDegreesHM(d));
+				System.out.printf("%.2f\n", getDegreesHMS(d));
+				System.out.printf("%.2f\n", getDegreesM(d));
+				System.out.printf("%.2f\n", getDegreesMS(d));
+				System.out.printf("%.2f\n", getDegreesS(d));
+				System.out.printf("Разница между часовой и минутной стрелкой равна  %.2f%n",
+						getDegreesBetwenHourAndMinute(d));
+				System.out.printf("Разница между минутной и секундной стрелкой равна %.2f%n",
+						getDegreesBetwenMinuteAndSecond(d));
+				System.out.printf("Разница между часовой и секундной стрелкой равна %.2f%n",
+						getDegreesBetwenHourAndSecond(d));
+			} else {
+				System.out.println(sTime + " - Неизвестный формат времени!");
+				main(null);
 			}
-			System.out.println(iii);
-//			while (m1.find()) {
-//				   ii2.add(Integer.valueOf(m1.group()));
-//				}
-			System.out.println(ii2);
-			
-//			if (!m.matches()) {
-//				System.out.println(s + " - Неизвестный формат времени!");
-//			} else {
-//				/*int[] iii = */Arrays.stream(s.split("[ :\\.,]*|\\D*"))
-//						.mapToInt(Integer::parseInt)
-//						/*.toArray(); //Exception in thread "main" java.lang.NumberFormatException: For input string: "" */
-//						.forEach(System.out::println);
-//				/*for (int i = 0; i < iii.length; i++) {
-//					System.out.println(iii[i]);
-//				}
-//				System.out.println(Arrays.toString(iii));*/
-//				
-////				String[] sss = ss[i].split("[ :\\.,]{1}\\");
-////				System.out.println(Arrays.toString(sss));
-//////				for (int i = 0; i < sss.length; i++) {
-//////					iii[i] = Integer.parseInt(sss[i]);
-//////				}
-////			}
-//			}
-//			
-////			String str = br.readLine();
-////			hour = (int) time;
-////			minute = 30;
-////			minuteDegrees = getDegrees(minute);
-////			hourDegrees = getDegrees(time);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-//		System.out.println(hour);
-//		System.out.println(minute);
-//		System.out.println(minuteDegrees);
-//		System.out.println(hourDegrees);
-
 	}
 
-	private static double getDegreesH;
-	private static double getDegreesHM;
-	private static double getDegreesHMS;
-	private static double getDegreesM;
-	private static double getDegreesMS;
-	private static double getDegreesS;
-
-	public static void getDegreesBetwenHourAndMinute(Long time) {
-
+	@SuppressWarnings("deprecation")
+	public static double getDegreesH(Date date) {
+		// в любом случае, чаовая стрелка проходит циферблат от 0 часов (включая) до 12
+		// часов (не включая)
+		return (date.getHours() > 12) ? ((date.getHours() - 12) * 30) : (date.getHours() * 30.0);
 	}
 
-	public static void getDegreesBetwenMinuteAndSecond(Long time) {
-
+	@SuppressWarnings("deprecation")
+	public static double getDegreesM(Date date) {
+		return date.getMinutes() * 6.0;
 	}
 
-	public static void getDegreesBetwenHourAndSecond(Long time) {
+	@SuppressWarnings("deprecation")
+	public static double getDegreesS(Date date) {
+		return date.getSeconds() * 6.0;
+	}
 
+	public static double getDegreesHM(Date date) {
+		// часовая стрелка движется в 12 раз медленее минутной
+		return (getDegreesH(date) + getDegreesM(date) / 12);
+	}
+
+	public static double getDegreesHMS(Date date) {
+		// часовая стрелка движется в 12 раз медленее минутной
+		// минутная стрелка движется в 60 раз медленнее секундной
+		return (getDegreesH(date) + (getDegreesM(date) / 12) + (getDegreesS(date) / (12 * 60)));
+	}
+
+	public static double getDegreesMS(Date date) {
+		// минутная стрелка движется в 60 раз медленнее секундной
+		return getDegreesM(date) + getDegreesS(date) / 60;
+	}
+
+	public static double getDegreesBetwenHourAndMinute(Date date) {
+		// return (Math.abs(getDegreesHMS(date) - getDegreesMS(date)));
+		return (getDegreesHMS(date) > getDegreesMS(date)) 
+				? (360 - getDegreesHMS(date) + getDegreesMS(date))
+				: (getDegreesMS(date) - getDegreesHMS(date));
+	}
+
+	public static double getDegreesBetwenMinuteAndSecond(Date date) {
+		// return (Math.abs(getDegreesMS(date) - getDegreesS(date)));
+		return (getDegreesMS(date) > getDegreesS(date)) 
+				? (360 - getDegreesMS(date) + getDegreesS(date))
+				: (getDegreesS(date) - getDegreesMS(date));
+	}
+
+	public static double getDegreesBetwenHourAndSecond(Date date) {
+		// return (Math.abs(getDegreesHMS(date) - getDegreesS(date)));
+		return (getDegreesHMS(date) > getDegreesS(date)) 
+				? (360 - getDegreesHMS(date) + getDegreesS(date))
+				: (getDegreesS(date) - getDegreesHMS(date));
 	}
 }
