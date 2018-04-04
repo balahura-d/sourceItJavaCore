adb1d52a68653ef43ee1815b68d682a6a200b338
Student <b@d.v>
2018-04-04 20:37:20 +0300

    passed first part of lesson 12
    We learned Generic and Optional

---
+++ src/lesson12/ClassWork12.java
@@ -0,0 +1,12 @@
+package lesson12;
+
+public class Classwork12 {
+
+        public static void main(String[] args) {
+                // ## Параметризация ##
+                //тема раскрыта в src.generic
+
+                //## Optional ##
+                Optional<Object> oo = Optional.empty;
+                System.out.println(oo.isPresent());        }
+}
